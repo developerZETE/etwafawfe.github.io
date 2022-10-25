1f47d57293223973adaf64e88de7460925441309
developerZETE <116651785+developerZETE@users.noreply.github.com>
2022-10-25 17:08:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["Моё имя Валерий", "Я сиська.админ", "Люблю 1с(нет)"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
